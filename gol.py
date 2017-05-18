4752bdde4a0a07fef14c668979cf0b71b2c1c4b0
John Verghese <john1.verghese@gmail.com>
2017-05-17 23:05:26 -0400

    finished, doesn't work on edges and can't pass specified array

--- gol.py
+++ gol.py
@@ -1,11 +1,54 @@
 #!/usr/bin/python3
+import os
+import time
+import random
 
+
+board_size = 100
+board = [[int((random.random() + 0.5) // 1) for i in range(board_size)] for j in range(board_size)]
 def main():
-    print("main")
+    clear_term()
+    while True: tick()
 
-def print_block():
-    s = '\u2598'
-    print(s)
 
+def clear_term():
+    os.system('cls' if os.name == 'nt' else 'clear') 
+
+def print_board():
+    s = '\u2588'
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == True:
+                print(s, end="")
+            else:
+                print(' ', end="")
+        print()
+
+def wrapped_100(n):
+    return 100 % n
+def update_board():
+    for i in range(len(board) -1):
+        for j in range(len(board) - 1):
+            neighbor_ct = ( board[i+1][j] + board[i][j+1] +
+                             board[i+1][j+1] + board[i-1][j]+
+                             board[i][j-1] + board[i-1][j-1]+
+                             board[i+1][j-1] + board[i-1][j+1] )
+
+            if neighbor_ct < 2:
+                board[i][j] = 0
+            elif neighbor_ct == 3:
+                board[i][j] = 1
+            elif neighbor_ct > 3:
+                board[i][j] = 0
+            else:
+                pass
+                #on exactly two, cells should remain as is
+
+def tick():
+    update_board()
+    print_board()
+    time.sleep(.1)
+
+
+print(board)
 main()
-print_block()
